ccdd767fb7a9c18eb386c1ca36b7a13429e0de7b
vinicius <vinicius.suga@gmail.com>
2022-10-19 16:03:58 -0300

    alterado o armazenamento dos xpath's

--- crawler.py
+++ crawler.py
@@ -6,19 +6,20 @@ class Cardapio:
     def __init__(self,driver):
         self.driver = driver
         self.url = "http://www.ru.uem.br/cardapio-1"
-        self.pratosegunda = '/html/body/div[1]/div[2]/div[1]/div[2]/div[5]/div/div[4]/table/tbody/tr[1]/td/p[3]'  #Xpath
-        self.pratoterca = '/html/body/div[1]/div[2]/div[1]/div[2]/div[5]/div/div[7]/table/tbody/tr[1]/td/p[3]'  #Xpath
-        self.pratoquarta = '/html/body/div[1]/div[2]/div[1]/div[2]/div[5]/div/div[9]/table/tbody/tr[1]/td/p[3]'  #Xpath
-        self.pratoquinta = '/html/body/div[1]/div[2]/div[1]/div[2]/div[5]/div/div[12]/table/tbody/tr[1]/td/p[3]'  #Xpath
-        self.pratosexta = '/html/body/div[1]/div[2]/div[1]/div[2]/div[5]/div/div[13]/table/tbody/tr[1]/td/p[3]'  #Xpath
+        self.lista = ['4','7','9','12','13']
+        self.inicio = '/html/body/div[1]/div[2]/div[1]/div[2]/div[5]/div/div['
+        self.fim = ']/table/tbody/tr[1]/td/p[3]'
+
 
     def navigate (self):
         self.driver.get(self.url)
 
 
     def getPrato(self):
-        semana = []
-        pratos = [self.pratosegunda,self.pratoterca,self.pratoquarta,self.pratoquinta,self.pratosexta]
+        semana,pratos = [],[]
+        for i in self.lista:
+            xpath = self.inicio + i + self.fim
+            pratos.append(xpath)
         for i in pratos:
             pratodia = self.driver.find_element(By.XPATH,i).text
             semana.append(pratodia)
