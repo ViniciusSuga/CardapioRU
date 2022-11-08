62661aee66731f85e466964169321578bdc78db9
Vinicius Suga <80865200+ViniciusSuga@users.noreply.github.com>
2022-11-08 11:22:49 -0300

    Delete bot.py

--- bot.py
+++
@@ -1,9 +0,0 @@
-import crawler_selenium
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-driver = webdriver.Firefox()
-c = crawler_selenium.Cardapio(driver)
-c.navigate()
-pratos = c.getPrato()
-print (pratos)
